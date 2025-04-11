2e11f5778196b1626fabb96758874ad9e12b6b64
Arsenii Istlentev <ais@figaf.com>
2025-04-11 14:14:46 +0300

    IRT-4954 Implement BusinessDocumentsClient

---
+++ src/test/java/com/figaf/integration/tpm/client/integration/BusinessDocumentsClientTest.java
@@ -0,0 +1,70 @@
+package com.figaf.integration.tpm.client.integration;
+
+import com.figaf.integration.common.entity.RequestContext;
+import com.figaf.integration.common.factory.HttpClientsFactory;
+import com.figaf.integration.tpm.client.b2bscenario.BusinessDocumentsClient;
+import com.figaf.integration.tpm.data_provider.AgentTestDataProvider;
+import com.figaf.integration.tpm.data_provider.CustomHostAgentTestData;
+import com.figaf.integration.tpm.entity.Interchange;
+import com.figaf.integration.tpm.entity.InterchangePayloadData;
+import com.figaf.integration.tpm.entity.InterchangeRequest;
+import org.apache.commons.lang3.time.DateUtils;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static com.figaf.integration.tpm.utils.Constants.PARAMETERIZED_TEST_NAME;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BusinessDocumentsClientTest {
+
+    private static BusinessDocumentsClient businessDocumentsClient;
+
+    @BeforeAll
+    static void setUp() {
+        businessDocumentsClient = new BusinessDocumentsClient(new HttpClientsFactory());
+    }
+
+    @ParameterizedTest(name = PARAMETERIZED_TEST_NAME)
+    @ArgumentsSource(AgentTestDataProvider.class)
+    void test_searchInterchanges(CustomHostAgentTestData agentTestData) {
+        RequestContext requestContext = agentTestData.createRequestContext(agentTestData.getTitle());
+        requestContext.getConnectionProperties().setHost(agentTestData.getIntegrationSuiteHost());
+
+        Date date = DateUtils.addDays(new Date(), -1);
+
+        InterchangeRequest interchangeRequest = new InterchangeRequest();
+        interchangeRequest.setOverallStatus("COMPLETED");
+        interchangeRequest.setAgreedSenderIdentiferAtSenderSide("GIRAFT");
+        interchangeRequest.setAgreedSenderIdentiferQualifierAtSenderSide("14");
+        interchangeRequest.setAgreedReceiverIdentiferAtSenderSide("KUNDENET");
+        interchangeRequest.setAgreedReceiverIdentiferQualifierAtSenderSide("ZZ");
+        interchangeRequest.setAgreedSenderIdentiferAtReceiverSide("GIRAF_IDOC_FILE");
+        interchangeRequest.setAgreedSenderIdentiferQualifierAtReceiverSide("GS1");
+        interchangeRequest.setAgreedReceiverIdentiferAtReceiverSide("FIGAF_IDOC_FILE");
+        interchangeRequest.setAgreedReceiverIdentiferQualifierAtReceiverSide("GS1");
+        interchangeRequest.setSenderAdapterType("Process_Direct");
+        interchangeRequest.setSenderDocumentStandard("ASC-X12");
+        interchangeRequest.setSenderMessageType("850");
+        interchangeRequest.setReceiverDocumentStandard("GS1_XML");
+        interchangeRequest.setReceiverMessageType("ORDERS.ORDERS05");
+
+        List<Interchange> interchanges = businessDocumentsClient.searchInterchanges(requestContext, date, interchangeRequest);
+        assertThat(interchanges).isNotEmpty();
+
+        for (Interchange interchange : interchanges) {
+            List<byte[]> payloads = new ArrayList<>();
+            for (InterchangePayloadData interchangePayloadData : interchange.getInterchangePayloadDataList()) {
+                payloads.add(businessDocumentsClient.downloadPayload(requestContext, interchangePayloadData.getPayloadId()));
+            }
+            assertThat(payloads).isNotEmpty();
+            assertThat(interchange.getInterchangePayloadDataList().get(0).getEventType()).isEqualTo("BUSINESSDOCUMENT_CREATE_EVENT");
+        }
+
+    }
+
+}
