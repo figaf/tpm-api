9ec551444d63709d06eef6111c935aa14ffb3245
Arsenii Istlentev <ais@figaf.com>
2026-01-19 19:44:22 +0300

    IRT-5515 Implement resolveLatestAdministrativeData for AgreementTemplateMetadata

--- src/main/java/com/figaf/integration/tpm/entity/AgreementTemplateMetadata.java
+++ src/main/java/com/figaf/integration/tpm/entity/AgreementTemplateMetadata.java
@@ -11,4 +11,16 @@ public class AgreementTemplateMetadata extends TpmObjectMetadata {
 
     private String b2bScenarioDetailsId;
     private AdministrativeData b2bScenarioDetailsAdministrativeData;
+
+    public AdministrativeData resolveLatestAdministrativeData() {
+        AdministrativeData agreementTemplateAdministrativeData = getAdministrativeData();
+        if (b2bScenarioDetailsAdministrativeData == null) {
+            return agreementTemplateAdministrativeData;
+        }
+
+        return agreementTemplateAdministrativeData.getModifiedAt().after(b2bScenarioDetailsAdministrativeData.getModifiedAt())
+            ? agreementTemplateAdministrativeData
+            : b2bScenarioDetailsAdministrativeData;
+    }
+
 }
